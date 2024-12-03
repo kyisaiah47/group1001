fc7b432117524f500709d746d06ac2bd2b0a1fb0
Isaiah <isaiah.kim@sscinc.com>
2024-12-03 18:49:26 -0500

    incorporate theme colors into components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
-
-// Import components
 import { StarshipsComponent } from './pages/starships/starships.component';
+import { HomeComponent } from './pages/home/home.component';
 
-// Define routes
 export const routes: Routes = [
-  { path: '', redirectTo: 'starships', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'starships', component: StarshipsComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 export const appRouterProvider = provideRouter(routes);
