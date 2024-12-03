d2ce143c0bcbfaed73a2e3ff446aca5e92e36e6b
Isaiah <isaiah.kim@sscinc.com>
2024-12-03 16:12:57 -0500

    Add styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { provideRouter } from '@angular/router';
 
 // Import components
 import { StarshipsComponent } from './pages/starships/starships.component';
-import { FilterComponent } from './pages/filter/filter.component';
 
 // Define routes
 export const routes: Routes = [
   { path: '', redirectTo: 'starships', pathMatch: 'full' },
   { path: 'starships', component: StarshipsComponent },
-  { path: 'filter', component: FilterComponent },
 ];
 
 export const appRouterProvider = provideRouter(routes);
--- src/app/pages/filter/filter.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-filter',
-  standalone: true,
-  templateUrl: './filter.component.html',
-  styleUrl: './filter.component.scss',
-})
-export class FilterComponent {}
